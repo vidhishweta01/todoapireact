1350f03ad78abbcc6c3d650566d458959d4b8f62
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-24 14:35:42 +0530

    added todo todolist to component created link in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
+import todosList from './component/todoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+      <Route path="/" component={todosList} exact />
       </Switch>
     </div>
   );
--- src/component/todoList/todoList.js
+++ src/component/todoList/todoList.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { FetchData } from '../../redux/action/todoAction';
+import Todo from '../todo/todo';
 
-const todo = () => {
+const todosList = () => {
     const dispatch = useDispatch();
     const state = useSelector((state) => state.TodoReducer);
 
@@ -13,10 +14,20 @@ const todo = () => {
     useEffect(() => {
     }, [state]);
 
-    const rendertodo = () => {
-        if (state.loading) {
-          return <h1>loading...</h1>;
-        }
-    
+  const rendertodo = () => {
+    if (state.loading) {
+      return <h1>loading...</h1>;
+    }
 
-}
+    if (state.items.length > 0) {
+      const todos = state.items;
+      return todos.map((todo, id) => {  // eslint-disable-line
+         <Todo id={id} todo={todo} />;
+      });
+    }
+    return <h1>cannot get todos list try again</h1>;
+  };
+  return (<div>{rendertodo()}</div>);
+};
+
+export default todosList;
