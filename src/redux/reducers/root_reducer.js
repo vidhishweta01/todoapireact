8551b68d5de993a0d143b678cf2929f0d91b41ec
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-25 13:33:03 +0530

    added PostDataReducer

--- src/redux/reducers/root_reducer.js
+++ src/redux/reducers/root_reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todo_reducer';
-
+import PostDataReducer from './post_data_reducer';
 const rootReducer = combineReducers({
   todoReducer,
+  PostDataReducer,
 });
 
 export default rootReducer;
