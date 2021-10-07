80ac5b218ba0227412cc4b76da51ffbb27222588
vidhishweta01 <vidhishweta01@gmail.com>
2021-10-07 22:14:57 +0530

    removed linter errors

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import ItemList from './component/itemslist/itemslist';
 import './App.css';
 
 function App() {
-  
   return (
     <div className="App">
       <Navbar />
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -25,9 +25,3 @@ export const IsSignedIn = () => {
   }
   return 'unauthorized action please sign in first!';
 };
-
-export const windowReload = () => {
-  const { RegisterationReducer } = store.getState();
-  if (!RegisterationReducer.Item) return true;
-  return false;
-};
